622930728e8a6eb0cdc369c0cba73fcf4b167b8b
Unanoc <leedaniil98@gmail.com>
2019-03-25 16:07:39 +0300

    some fixes

--- pkg/instagram.go
+++ pkg/instagram.go
@@ -1,9 +1,6 @@
 package pkg
 
 import (
-	"log"
-	"os"
-
 	"gopkg.in/ahmdrz/goinsta.v2"
 )
 
@@ -11,7 +8,6 @@ import (
 func CreateInstagram(username, password string) (*Instagram, error) {
 	var instagram Instagram
 	if err := instagram.SetAccount(username, password); err != nil {
-		log.Println(err)
 		return nil, err
 	}
 
@@ -27,7 +23,6 @@ type Instagram struct {
 func (i *Instagram) SetAccount(username, password string) error {
 	i.Account = goinsta.New(username, password)
 	if err := i.Account.Login(); err != nil {
-		log.Println(err)
 		return err
 	}
 
@@ -38,7 +33,6 @@ func (i *Instagram) SetAccount(username, password string) error {
 func (i *Instagram) GetUserByUsername(username string) (*goinsta.User, error) {
 	user, err := i.Account.Profiles.ByName(username)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
 
@@ -49,7 +43,6 @@ func (i *Instagram) GetUserByUsername(username string) (*goinsta.User, error) {
 func (i *Instagram) GetUnfollowedUsers(username string) (*[]string, error) {
 	user, err := i.GetUserByUsername(username)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
 
@@ -72,25 +65,4 @@ func (i *Instagram) GetUnfollowedUsers(username string) (*[]string, error) {
 	}
 
 	return &result, nil
-}
-
-// GetUnfollowedUsersFile ...
-func (i *Instagram) GetUnfollowedUsersFile(username, path string) error {
-	users, err := i.GetUnfollowedUsers(username)
-	if err != nil {
-		return err
-	}
-
-	f, err := os.OpenFile(path,
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	for _, user := range *users {
-		f.WriteString(user + "\n")
-	}
-
-	return nil
 }
