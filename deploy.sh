99d41979e206e11dcf376f37bfc85228644a60d1
Daniil Lee <d.lee@corp.mail.ru>
2019-09-11 23:51:57 +0300

    deleted all code from project

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker rm instabot -f
-docker rmi $(docker images | grep none | awk '{print $3}')
-docker build . -t instabot
-docker run --name instabot -v $1:/go/src/github.com/Unanoc/InstaFollower/logs -d instabot
-docker ps
